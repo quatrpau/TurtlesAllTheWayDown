72cfaffd376c8c8d89ce9ff410a9b3b8e81f92bf
Quatrani Paul <quatranipaul@zipcodes-MacBook-Pro-9.local>
2021-12-04 20:59:26 -0500

    Finished factorial

--- src/main/java/Turtles.java
+++ src/main/java/Turtles.java
@@ -1,7 +1,28 @@
-package PACKAGE_NAME;
 
 /**
  * Created by kristofer on 7/14/20.
  */
 public class Turtles {
+    public static int factorial(int multiplier){
+        if(multiplier == 0)return 0;
+        else if(multiplier == 1)return 1;
+        else{
+            return multiplier * factorial(multiplier - 1);
+        }
+    }
+    //one or two must not be 0 //denom always starts at 0
+    public static int greatestCommonDenominator(int one, int two, int denom){
+        if(one == two){
+            return one *
+        }
+        else if(one % 2 == 0 && two % 2 == 0){
+            return greatestCommonDenominator(one / 2, two / 2, denom + 1);
+        }
+        else if(one % 2 == 0){
+            return greatestCommonDenominator(one /2, two, denom);
+        }
+        else if(two % 2 == 0){
+            return greatestCommonDenominator(one, two/2, denom);
+        }
+    }
 }
--- src/test/java/TurtlesTest.java
+++ src/test/java/TurtlesTest.java
@@ -1,8 +1,29 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by kristofer on 7/14/20.
  */
 public class TurtlesTest {
+    @Test
+    public void factorial0(){
+        Assert.assertEquals(0,Turtles.factorial(0));
+    }
+    @Test
+    public void factorial1(){
+        Assert.assertEquals(1,Turtles.factorial(1));
+    }
+    @Test
+    public void factorial2(){
+        Assert.assertEquals(2,Turtles.factorial(2));
+    }
+    @Test
+    public void factorial3(){
+        Assert.assertEquals(6,Turtles.factorial(3));
+    }
+
+
 
 }
